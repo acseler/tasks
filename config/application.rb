77e0369d5dd63a4ee3bc357c4c172bae7e81e835
acsler <acsler@mail.ru>
2016-09-05 16:32:03 +0300

    Deploying

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module TodoList
     end
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    config.assets.initialize_on_precompile = false
   end
 end
