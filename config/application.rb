90684a8e07ba9ff452280af52fc46611395a003c
acsler <acsler@mail.ru>
2016-09-05 18:04:09 +0300

    Deploying

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,6 @@ module TodoList
     end
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
